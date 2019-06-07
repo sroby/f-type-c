098a89c4e106367e697eb1f63c4dabec17788e60
Simon Roby <simon.roby@gmail.com>
2019-06-06 22:35:55 -0400

    Initial Commit

---
+++ f-type/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  f-type
+//
+//  Created by Simon Roby on 2019-06-06.
+//  Copyright © 2019 Simon Roby. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
